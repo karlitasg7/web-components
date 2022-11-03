5c0d738c4e7327706c8c12de1b8b9d4c2ad51c41
Karla Sequen <karlasequen@gmail.com>
2022-11-02 19:01:29 -0600

    send info with atributes

---
+++ Attributes/my-element.js
@@ -0,0 +1,50 @@
+class myElement extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+
+        this.title = this.getAttribute('title');
+        this.body = this.getAttribute('body');
+        this.img = this.getAttribute('img');
+    }
+
+    getTemplate() {
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <section>
+                <h2>${this.title}</h2>
+
+                <div>
+                    <p>${this.body}</p>
+                    <img src=${this.img}/>
+                </div>
+            </section>
+
+            ${this.getStyles()}
+        `;
+
+        return template;
+    }
+
+    getStyles() {
+        return `
+        <style>
+        
+            h2 {
+                color: red;
+            }            
+        
+        </style>
+        `;
+    }
+
+    render() {
+        this.shadowRoot.appendChild(this.getTemplate().content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+}
+
+customElements.define('my-element', myElement);
